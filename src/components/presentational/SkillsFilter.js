fb10bbdb0c5b2426f7717c36da6b3eeecd5b651c
Abdullah Numan <anewman15@hotmail.com>
2021-03-06 17:54:05 +0600

    Create Loading comp

--- src/components/presentational/SkillsFilter.js
+++ src/components/presentational/SkillsFilter.js
@@ -2,6 +2,7 @@ const SkillsFilter = () => {
   const filter = 'Skill';
   return (
     <div>
+      <h3>Filter repositories by skills</h3>
       <form>
         <input type="text" placeholder={filter} />
         <button type="submit">Filter</button>
