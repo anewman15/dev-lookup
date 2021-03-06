75bfe79e6b34a41bc8d0075fcf22168de65db513
Abdullah Numan <anewman15@hotmail.com>
2021-03-06 16:31:52 +0600

    Create UserRepos comp and display it in App.js

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,10 @@
 import SearchUser from '../presentational/SearchUser';
+import UserRepos from './UserRepos';
 
 const App = () => (
   <div className="App">
     <SearchUser />
+    <UserRepos />
   </div>
 );
 
---
+++ src/components/containers/UserRepos.js
@@ -0,0 +1,11 @@
+const UserRepos = () => {
+  const text = 'Repositories';
+
+  return (
+    <div>
+      <h1>{`User's ${text}`}</h1>
+    </div>
+  );
+};
+
+export default UserRepos;
