9635571a930637953067274305114d2c02bd05e4
Abdullah Numan <anewman15@hotmail.com>
2021-03-06 18:21:17 +0600

    Create TopNav comp and display it in App.js

---
+++ src/components/presentational/TopNav.js
@@ -0,0 +1,14 @@
+const TopNav = () => (
+  <nav>
+    <div>
+      <div>
+        <img src="logo.png" alt="dev-lookup-logo" />
+      </div>
+      <div>
+        <p>Get anewman15&apos;s repos</p>
+      </div>
+    </div>
+  </nav>
+);
+
+export default TopNav;
