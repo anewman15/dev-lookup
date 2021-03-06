80e6d68d6a7a6002e78f8911ae665333118fdee4
Abdullah Numan <anewman15@hotmail.com>
2021-03-06 20:44:41 +0600

    Define handleSubmit func and call it onSubmit in form

--- src/components/presentational/SearchUser.js
+++ src/components/presentational/SearchUser.js
@@ -1,16 +1,24 @@
 import { useState } from 'react';
+import fetchData from '../../sandbox/fetchData';
 
 const SearchUser = () => {
   const [username, setUsername] = useState('anewman15');
+  const [userData, setUserData] = useState([]);
+  const uri = `https://api.github.com/users/${username}/repos`;
 
   const handleChange = e => {
     setUsername(e.target.value);
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetchData(uri, username, setUserData);
+  };
+
   return (
     <div>
-      <h1>Look up a developer on GitHub</h1>
-      <form>
+      <h1>Look up a username on GitHub</h1>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="anewman15" value={username} onChange={handleChange} />
         <button type="submit">Search</button>
       </form>
