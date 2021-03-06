4d8578e1cc3ba7520654dba7b1e10fd9710dbf11
Abdullah Numan <anewman15@hotmail.com>
2021-03-06 19:16:56 +0600

    Create handleChange function and call it from input element in SearchUser

--- src/components/presentational/SearchUser.js
+++ src/components/presentational/SearchUser.js
@@ -3,11 +3,15 @@ import { useState } from 'react';
 const SearchUser = () => {
   const [username, setUsername] = useState('anewman15');
 
+  const handleChange = e => {
+    setUsername(e.target.value);
+  };
+
   return (
     <div>
       <h1>Look up a developer on GitHub</h1>
       <form>
-        <input type="text" placeholder="anewman15" value={username} onClick={handleChange} />
+        <input type="text" placeholder="anewman15" value={username} onChange={handleChange} />
         <button type="submit">Search</button>
       </form>
     </div>
