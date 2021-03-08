d62db763061d5b6a2b201ceade91f97cbb903c20
Abdullah Numan <anewman15@hotmail.com>
2021-03-08 15:25:09 +0600

    Define filter reducer

---
+++ src/redux/reducers/filter.js
@@ -0,0 +1,17 @@
+const filter = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_FILTER': {
+      return {
+        ...state,
+        filters: {
+          skill: action.payload.filter,
+        },
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default filter;
