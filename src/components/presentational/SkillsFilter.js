acf35298bdb9d4ff38cd6d37bbe592f4d67f246b
Abdullah Numan <anewman15@hotmail.com>
2021-03-08 15:32:46 +0600

    Define handleChange and make input field controlled

--- src/components/presentational/SkillsFilter.js
+++ src/components/presentational/SkillsFilter.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 const SkillsFilter = () => {
-  const filter = 'Skill';
+  const [filter, setFilter] = useState('Ruby');
+
+  const handleChange = e => {
+    setFilter(e.target.value);
+  };
+
+  console.log(filter);
+
   return (
     <div>
-      <h3>Filter repositories by skills</h3>
+      <h3>Filter by skills</h3>
       <form>
-        <input type="text" placeholder={filter} />
+        <input type="text" placeholder="Ruby" value={filter} onChange={handleChange} />
         <button type="submit">Filter</button>
       </form>
     </div>
