a2269e51f546c9ff4261aac7b0aab677e5628d4e
Abdullah Numan <anewman15@hotmail.com>
2021-03-09 19:27:37 +0600

    Create css rules for input and button and Style SearchUser component

--- src/components/presentational/SearchUser.js
+++ src/components/presentational/SearchUser.js
@@ -1,17 +1,15 @@
 import PropTypes from 'prop-types';
+import common from '../../styles/commonStyles.module.css';
 
-const SearchUser = ({ username, handleChange, handleSubmit }) => {
-  const userRepos = 'Hi';
-  return (
-    <div>
-      <h1>{`Look up a username on GitHub ${userRepos}`}</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="anewman15" value={username} onChange={handleChange} />
-        <button type="submit">Search</button>
-      </form>
-    </div>
-  );
-};
+const SearchUser = ({ username, handleChange, handleSubmit }) => (
+  <div className={`${common.textCenter}`}>
+    <h1>Look up a username on GitHub</h1>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="anewman15" value={username} onChange={handleChange} />
+      <button type="submit">Search</button>
+    </form>
+  </div>
+);
 
 SearchUser.propTypes = {
   handleChange: PropTypes.function,
