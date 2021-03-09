17bf9bed9396264afd17d77a7b35e408fd659bbc
Abdullah Numan <anewman15@hotmail.com>
2021-03-09 16:52:14 +0600

    Use selector to return userRepos and render SkillsFilter in UserRepos comp

--- src/components/containers/UserRepos.js
+++ src/components/containers/UserRepos.js
@@ -1,23 +1,31 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import selectFilteredRepos from '../../redux/selectors/selectFilteredRepos';
+import SkillsFilter from '../presentational/SkillsFilter';
 import Repo from './Repo';
 
-const UserRepos = ({ userRepos }) => {
+const UserRepos = ({ username, userRepos }) => {
   const repos = userRepos.map((repo, index) => <Repo key={repo.id} repo={repo} index={index} />);
 
   return (
     <div>
+      <h2>
+        {username}
+        &apos; Repositories
+      </h2>
+      <SkillsFilter />
       {repos}
     </div>
   );
 };
 
 UserRepos.propTypes = {
+  username: PropTypes.string,
   userRepos: PropTypes.array,
 }.isRequired;
 
 const mapStateToProps = state => ({
-  userRepos: [...state.userRepos],
+  userRepos: selectFilteredRepos(state),
 });
 
 export default connect(mapStateToProps)(UserRepos);
