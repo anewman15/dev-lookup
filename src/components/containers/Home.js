d1ff85e87584d5def9eb52dfed6cbed264949078
Abdullah Numan <anewman15@hotmail.com>
2021-03-10 15:21:54 +0600

    Remove SearchFilter comp Home.js

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,9 +1,7 @@
-import SearchUser from '../presentational/SearchUser';
 import UserInfo from './UserInfo';
 
 const Home = () => (
   <div className="Home">
-    <SearchUser />
     <UserInfo />
   </div>
 );
