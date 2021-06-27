1ad67aa8e13bda5f1c45fff9e69b85c6234a830a
Abdullah Numan <anewman15@hotmail.com>
2021-06-27 11:05:26 +0600

    Apply formatted time to RepoDetails comp

--- src/utils/date.js
+++ src/utils/date.js
@@ -4,4 +4,4 @@ import {
 
 export const formattedDate = time => format(new Date(parseISO(time)), 'MMM dd, yyyy, HH:mm');
 
-export const commitTime = time => formatDistanceToNow(new Date(parseISO(time)));
+export const timeFromNow = time => formatDistanceToNow(new Date(parseISO(time)));
